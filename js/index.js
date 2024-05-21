eae715f19bff52c1f8d6cc51c2df4cd8bf40d88f
alanleonelfernandez <alanleonelfernandez@hotmail.com>
2024-05-21 19:07:44 -0300

    conexion a la api

--- js/index.js
+++ js/index.js
@@ -13,6 +13,59 @@ document.addEventListener('DOMContentLoaded', function() {
           this.parentNode.classList.toggle('open');
       });
   });
+
+//difuminado de secciones
+  var seccion1 = document.querySelector('.seccion-1');
+  var seccion2 = document.querySelector('.seccion-2');
+  function isInViewport(element) {
+      var rect = element.getBoundingClientRect();
+      return (
+          rect.top >= 0 &&
+          rect.left >= 0 &&
+          rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+          rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      );
+  }
+  function mostrarSeccion() {
+      if (isInViewport(seccion1)) {
+          seccion1.classList.add('aparecer');
+      }
+      if (isInViewport(seccion2)) {
+          seccion2.classList.add('aparecer');
+      }
+  }
+  window.addEventListener('scroll', mostrarSeccion);
+  mostrarSeccion();
+
+  // Llamada a Api de Dólar
+  fetch("https://dolarapi.com/v1/dolares")
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    data.forEach(dolar => {
+        const dolarElement = document.createElement('div');
+        dolarElement.classList.add('dolar-item');
+        const dolarTipo = document.createElement('h3');
+        dolarTipo.textContent = `Dolar ${dolar.nombre} :`;
+        const dolarPrecio = document.createElement('p');
+        dolarPrecio.textContent = `Compra: $${dolar.compra} - Venta: $${dolar.venta}`;
+        dolarElement.appendChild(dolarTipo);
+        dolarElement.appendChild(dolarPrecio);
+        document.getElementById('dolar-container').appendChild(dolarElement);
+    });
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+    const errorElement = document.createElement('div');
+    errorElement.textContent = 'No se pudo cargar el valor del Dolar.';
+    document.getElementById('dolar-container').appendChild(errorElement);
+  });
+
+  //Fecha actual
+  var today = new Date();
+  var options = { year: 'numeric', month: 'long', day: 'numeric' };
+  var formattedDate = today.toLocaleDateString('es-ES', options);
+  document.getElementById('current-date').textContent = formattedDate;
 });
 
 //boton volver arriba
