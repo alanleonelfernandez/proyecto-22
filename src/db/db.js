85d8431ac01ceb0cb7692789bb657a128645b470
alanleonelfernandez <87732620+alanleonelfernandez@users.noreply.github.com>
2024-07-12 18:12:10 -0300

    port de db

--- src/db/db.js
+++ src/db/db.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv').config();
 
 const connection = mysql.createConnection({
     host: process.env.HOST_DB,
+    port: process.env.PORT_DB,
     user: process.env.USER_DB,
     password: process.env.PASS_DB,
     database: process.env.NAME_DB
